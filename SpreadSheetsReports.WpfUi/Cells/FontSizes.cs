a1b30c21b73d16fca48295df80950819ef83a69c
Ricardo Escobar <rbarrios00@gmail.com>
2017-05-25 21:23:42 -0600

    Additional designer features

---
+++ SpreadSheetsReports.WpfUi/Cells/FontSizes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadSheetsReports.WpfUi.Cells
+{
+    public class FontSizesList
+    {
+        public int[] GetFontSizes()
+        {
+            var intList = new List<int>();
+            for (int i = 8; i <= 12; i++)
+            {
+                intList.Add(i);
+            }
+
+            for (int i = 14; i <= 28; i++)
+            {
+                intList.Add(i);
+            }
+
+            intList.Add(36);
+            intList.Add(48);
+            intList.Add(72);
+
+            return intList.ToArray();
+        }
+    }
+}
